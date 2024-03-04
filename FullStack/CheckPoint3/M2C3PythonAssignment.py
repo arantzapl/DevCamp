a978f21cfd26a4ff50ace90239dc2dc31e95faa7
arantzapl <arantza.perezlaborda@gmail.com>
2024-03-05 00:22:41 +0100

    CheckPoint3

---
+++ FullStack/CheckPoint3/M2C3PythonAssignment.py
@@ -0,0 +1,60 @@
+# Exercise 1
+
+str_ex1 = 'Create a string'
+num_ex1 = 30
+lis_ex1 = ['hello', 5, 'goodbye', 2]
+boo_ex1 = True
+
+print(str_ex1)
+print(num_ex1)
+print(lis_ex1)
+print(boo_ex1)
+
+# Exercise 2
+
+str_ex2 = 'Exercise 2 str variable'
+str_exx2 = str_ex2[:3]
+print(str_exx2)
+
+# Exercise 3
+
+lis_ex3 = ['First', 'second', 'third']
+lis_exx3 = lis_ex3[0]
+print(lis_exx3)
+
+# Exercise 4
+
+num_ex4 = 29
+num_exx4 = num_ex4 + 10
+print(num_exx4)
+
+# Exercise 5
+
+lis_ex5 = ['Element1' , 'Element2' , 'Element3' , 'Element4' , 'Element5']
+lis_exx5 = lis_ex5[-1]
+print(lis_exx5)
+
+# Exercise 6
+
+names = 'harry, alex, susie, jared, gail, conner'
+names_two = names.split(',')
+print(names_two)
+
+# Exercise 7
+
+str_ex7 = "Exercise 7 first word"
+str_exx7 = str_ex7.index(" ")
+str_exxx7= str_ex7[:str_exx7].upper()
+new_str = str_exxx7 + str_ex7[str_exx7:]
+print(new_str)  
+
+# Exercise 8
+
+str_ex8 = 'My favorite number is'
+num_ex8 = 8
+str_num_ex8 = f'{str_ex8} {num_ex8}.'
+print(str_num_ex8)
+
+# Exercise 9
+
+print('Hello world')
