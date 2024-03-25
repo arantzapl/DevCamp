03bfffdac740ad3ac0cd9a0bb84ec1e6d6381627
arantzapl <arantza.perezlaborda@gmail.com>
2024-03-25 23:50:44 +0100

    Ejemplos de variables

---
+++ FullStack/Python/Variables/data_types_variables.py
@@ -0,0 +1,18 @@
+"""
+- Booleans
+- Numbers
+- Strings
+- Bytes and byte arrays
+- None
+- Lists
+- Tuples
+- Sets
+- Dictionaries
+"""
+
+meal_completed = True
+sub_total = 100
+tip = sub_total * 1/5
+total = sub_total + tip
+receipt = "Your total is " + str(total)
+print(receipt)
